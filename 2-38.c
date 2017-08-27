f1f50bc48989b4cab038ec7d7cfa7f3e8a9d7f4a
againgolffy <31381739+againgolffy@users.noreply.github.com>
2017-08-27 16:33:53 +0700

    2-38

---
+++ 2-38.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	printf("%s\n","58224090116");
+	printf("%s\n","Kalanyoo Jantong");
+	printf("%s\n","21/04/1995");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("%120s\n","http://www.kru.ac.th");
+	printf("\n");
+	printf("%57s%s","","BYE BYE");
+	return 0;
+}
